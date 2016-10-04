d3e7ada8a2f3cd648412d7de0d4773450b1ba82e
chgad <chgad.games@web.de>
2016-10-04 16:45:58 +0200

    Nächster Push

--- Git_class.py
+++ Git_class.py
@@ -10,14 +10,34 @@ class Account :
     '''
     accounts = 0
 
-    def __init__(self, name, pw , date, region):
+    def __init__(self, name, pw , date, region, mail):
         self.name = name
         self.date = date
         self.region = region
+        self.pw = pw
+        self.mail = mail
         Account.accounts += 1
         
-
-Sam = Account("Sam","123","01.01.00","europe")
+    def storing_pw(self):
+        store = open("PW_storage.txt", 'a')
+        store.write(self.pw)
+        store.close()
+        print("Password stored!")
+    def pw_request(self):
+        c= 0
+        while c < 3 :
+            nam= input("waht is your name?")
+            mai= input("waht is your mail address?")
+            if (nam == self.name) and mai == self.mail :
+                return print(self.pw)
+        
+            else :
+                c +=1
+                print("Access Denied! Try again. %i/3 Attempts" % c)
+        
+Sam = Account("Sam","123","01.01.00","europe","test@aox.com")
 print(Sam.accounts)
-Tina = Account("Tina","456","01.01.00","Asia")
+Tina = Account("Tina","456","01.01.00","Asia","test2@aox.com")
 print(Tina.accounts)
+Sam.storing_pw()
+Sam.pw_request()
